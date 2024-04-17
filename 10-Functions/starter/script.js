ac47536aa7853e9df3dafd5fc19ab9c92d1b6403
Daniel Bekenstein <dbekenstein@gmail.com>
2024-04-17 08:37:01 -0400

    4/17

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -324,8 +324,80 @@ const secureBooking = function () {
 // passengerCount is private
 // console.log(passengerCount);
 const booker = secureBooking();
+/*
+booker();
+booker();
+booker();
 
+console.dir(booker);
+*/
 // ----------------------------------------------------------------------
+// More Closure Examples
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 7;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+/*
+g();
+f();
+console.dir(f);
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+*/
+
+// Example 2
+// Timer
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  // timer - after 1000 ms, call the function
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+
+  // Will start boarding in ${wait seconds}
+  // Delay of wait * 1000
+  // CL of setTimeout function
+};
+// this will not be used, it will prioritize the variable inside the function.
+const perGroup = 10000;
+
+// boardPassengers(180, 3);
 // ----------------------------------------------------------------------
+// Coding Challenge 2
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
