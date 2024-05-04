9316a0739b9e0f2f147c67264e14c4506cb1928b
Daniel Bekenstein <dbekenstein@gmail.com>
2024-05-04 15:09:54 -0400

    finished DOM

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -529,6 +529,21 @@ console.log(h1.parentElement.children);
 // Tabbed Component
 
 // ---------------------------------------------------------------------
+//
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
+
 // ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
